28e0c4439c734a16304fe4611421bffec569f769
Jonas Hecht <jonas.hecht@codecentric.de>
2017-12-11 17:14:32 +0100

    Because from 1709 on, the nanoserver image doesn´t provide a PowerShell anymore, we need to switch to https://hub.docker.com/r/microsoft/powershell/ as base image. Therefore the docker-preparation playbooks now also support a tag for only building the base image with `--tags baseimage`

--- step4-windows-linux-multimachine-vagrant-docker-swarm-setup/playbookCommands.sh
+++ step4-windows-linux-multimachine-vagrant-docker-swarm-setup/playbookCommands.sh
@@ -1,6 +1,9 @@
 # Prepare Docker on all Cluster nodes
 ansible-playbook -i hostsfile prepare-docker-nodes.yml
 
+# Only prepare base image springboot-oraclejre-nanoserver
+ansible-playbook -i hostsfile prepare-docker-nodes.yml --tags "baseimage"
+
 # Initialize Docker Swarm
 ansible-playbook -i hostsfile initialize-docker-swarm.yml
 
