dd2251d4293ec6dcb68b2b03260ceca8d3f22aa8
Jonas Hecht <jonas.hecht@codecentric.de>
2017-12-19 14:49:00 +0100

    New tag dockerinstall for leaving out newest Docker installation process on Windows

--- step4-windows-linux-multimachine-vagrant-docker-swarm-setup/playbookCommands.sh
+++ step4-windows-linux-multimachine-vagrant-docker-swarm-setup/playbookCommands.sh
@@ -4,6 +4,9 @@ ansible-playbook -i hostsfile prepare-docker-nodes.yml
 # Only prepare base image springboot-oraclejre-nanoserver
 ansible-playbook -i hostsfile prepare-docker-nodes.yml --tags "baseimage"
 
+# Leave out newer Docker installation
+ansible-playbook -i hostsfile prepare-docker-nodes.yml --skip-tags "dockerinstall"
+
 # Initialize Docker Swarm
 ansible-playbook -i hostsfile initialize-docker-swarm.yml
 
