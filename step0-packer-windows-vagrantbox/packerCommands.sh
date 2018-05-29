021a6ccc4ca125e104176c51eda4fa2ab18afc48
Jonas Hecht <jonas.hecht@codecentric.de>
2018-05-29 10:30:17 +0200

    Adding Windows Server 1803 to the Packer build

--- step0-packer-windows-vagrantbox/packerCommands.sh
+++ step0-packer-windows-vagrantbox/packerCommands.sh
@@ -11,3 +11,12 @@ packer build -var iso_url=en_windows_server_version_1709_x64_dvd_100090904.iso -
 
 # add it to local Vagrant Boxes
 vagrant box add --name windows_1709_docker_multimachine windows_1709_docker_multimachine_virtualbox.box
+
+
+# Run packer build with Windows Server 1803
+packer build -var iso_url=en_windows_server_version_1803_x64_dvd_12063476.iso -var iso_checksum=e34b375e0b9438d72e6305f36b125406 -var template_url=vagrantfile-windows_1803-multimachine.template -var box_output_prefix=windows_1803_docker_multimachine windows_server_2016_docker.json
+
+# add it to local Vagrant Boxes
+vagrant box add --name windows_1803_docker_multimachine windows_1803_docker_multimachine_virtualbox.box
+
+
