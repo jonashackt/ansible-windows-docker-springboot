076f190f04258cae64a0e3bea76f5474a2723975
Jonas Hecht <jonas.hecht@codecentric.de>
2018-05-29 09:42:48 +0200

    Fixing "ValueError: No closing quotation" with Ansible ufw module on Ubuntu 16.04. - Also added tags for ease of use

--- step5-deploy-multiple-spring-boot-apps-to-mixed-os-docker-swarm/playbookCommands.sh
+++ step5-deploy-multiple-spring-boot-apps-to-mixed-os-docker-swarm/playbookCommands.sh
@@ -4,3 +4,6 @@ ansible-playbook -i hostsfile build-and-deploy-apps-2-swarm.yml
 # Just deploy all Applications to Swarm (they have to been build once before that will work out)
 ansible-playbook -i hostsfile build-and-deploy-apps-2-swarm.yml --skip-tags "buildapps"
 
+# Only open Firewall ports for Apps
+ansible-playbook -i hostsfile build-and-deploy-apps-2-swarm.yml --skip-tags="deploy,buildapps"
+
